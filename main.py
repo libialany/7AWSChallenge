f7a0c8b16de9f8bf36e0246904f74c90fa45bcb8
melanylibia <elibialany@gmail.com>
2025-04-12 23:31:31 -0400

    feat:  func verify result

--- main.py
+++ main.py
@@ -2,13 +2,24 @@ from pydantic import BaseModel
 from fastapi import FastAPI
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from fastapi.responses import JSONResponse
+import re    
 import os
+import requests
 import telebot
+text = ""
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 CHAT_ID = os.environ.get('CHAT_ID')
+URL_GITHUB="https://raw.githubusercontent.com"
 bot = telebot.TeleBot(BOT_TOKEN)
 Base = declarative_base()
-
+challenges = [
+    "Day1.md",
+    "Day2.md",      
+    "Day3.md",
+    "Day4.md",      
+    "Day5.md"
+]
 
 class ItemSchema(BaseModel):
     repo_name: str
@@ -28,8 +39,38 @@ class Item(Base):
 
 app = FastAPI()
 
+def is_content_completed(text: str):
+    pattern = r'result:\s*(https?://(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&//=]*))'
+    match = re.search(pattern, text)
+    status = match is not None
+    if status:
+        return True
+    else:
+        return False
+
+def is_challenge_completed(item: ItemSchema, challenge: str):
+    url = f"{URL_GITHUB}/{item.repo_name}/main/{challenge}"
+    response = requests.get(url)
+    if response.status_code == 404:
+        return False
+    text = response.content.decode('utf-8')
+    if not text:
+        return False
+    if not is_content_completed(text):
+        return False
+    return True
 def check_content(item: ItemSchema):
-    text= f"Repo name: {item.repo_name}\nGH username: {item.gh_username}\nContent repo: {item.content_repo}"
+    global challenges
+    global text
+    challenge_count = 0 
+    for challenge in challenges:
+        if is_challenge_completed(item, challenge):
+            text = "Felicidades! Has completado el desafío " + challenge
+            challenge_count += 1
+        else:
+            text = "No has completado el desafío " + challenge
+    if challenge_count == 5:
+        text = "Felicidades! Has completado todos los desafíos"
     bot.send_message(CHAT_ID, text)
 
 
@@ -38,8 +79,13 @@ def areSafeText(item: ItemSchema):
 
 @app.post("/api/todo")
 def addItem(item: ItemSchema):
-    todoitem = Item(repo_name=item.repo_name,gh_username=item.gh_username, content_repo=item.content_repo)
+    global text
     if areSafeText(item):
         check_content(item)
-        return todoitem
+        return JSONResponse(
+            status_code=200,
+            content={
+                "content": text
+            }
+        )
     return {"error": "Invalid input"}
